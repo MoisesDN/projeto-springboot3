3c49abc03e943ec3d217d95d55122453a2904a8c
MoisesDN <m.dias009@gmail.com>
2024-04-07 15:44:23 -0300

    feat: create Inteface ProductRepository

---
+++ src/main/java/com/moisesdias/springboot/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.moisesdias.springboot.repositories;
+
+import com.moisesdias.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
